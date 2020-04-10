2282ccbb7ec6bef5fba4894adf03b0ae349540b6
Ashraq Khan <akhan032@citymail.cuny.edu>
2020-04-10 18:34:55 -0400

    Final version for P3

--- p3/gol.cpp
+++ p3/gol.cpp
@@ -7,12 +7,13 @@
  * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
  * YOUR OWN WORK**.
  * References:
- *
+ * Dawen, Azwad, Matt, Kyle, readme.html
  *
  * Finally, please indicate approximately how many hours you spent on this:
- * #hours: 
+ * #hours: INT_MAX
  */
 
+#include <iostream>
 #include <cstdio>
 #include <stdlib.h> // for exit();
 #include <getopt.h> // to parse long arguments.
@@ -21,6 +22,7 @@
 using std::vector;
 #include <string>
 using std::string;
+using namespace std;
 
 static const char* usage =
 "Usage: %s [OPTIONS]...\n"
@@ -34,12 +36,10 @@ size_t max_gen = 0; /* if > 0, fast forward to this generation. */
 string wfilename =  "/tmp/gol-world-current"; /* write state here */
 FILE* fworld = 0; /* handle to file wfilename. */
 string initfilename = "/tmp/gol-world-current"; /* read initial state from here. */
-
-/* NOTE: you don't have to write these functions -- this is just how
- * I chose to organize my code. */
+vector<vector<bool> > W ;
 size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& g);
 void update();
-int initFromFile(const string& fname); /* read initial state into vectors. */
+void initFromFile(const string fname); /* read initial state into vectors. */
 void mainLoop();
 void dumpState(FILE* f);
 
@@ -78,28 +78,133 @@ int main(int argc, char *argv[]) {
 				return 1;
 		}
 	}
-	/* NOTE: at this point wfilename initfilename and max_gen
-	 * are all set according to the command line: */
-	printf("input file:  %s\n",initfilename.c_str());
-	printf("output file: %s\n",wfilename.c_str());
-	printf("fast forward to generation: %lu\n",max_gen);
+
 	/* TODO: comment out 3 lines above once you see what's in them. */
 	/* NOTE: if wfilename == "-" you should write to stdout, not a file
 	 * named "-"! */
 
-	/* If you wrote the initFromFile function, call it here: */
-	// initFromFile(initfilename);
+	initFromFile(initfilename);
 	mainLoop();
+
 	return 0;
 }
 
+size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& g)
+{
+	size_t counter = 0;
+	int r = g.size();
+	int c = g[0].size();
+	for (int x = -1; x <= 1; x++)
+	{
+		for (int y = -1; y <= 1; y++)
+		{
+			if ((g[(i + x + r)%r][(j + y + c)%c] == 1))
+			{
+				counter++;
+			}
+		}
+	}
+	if ((g[i][j]) == 1){
+		counter--;
+	}
+	return counter;
+}
+
+void update(){
+	vector<vector<bool> > newGen = W;
+	size_t r = W.size();
+	size_t c = W[0].size();
+	for (size_t i = 0; i < r; i++){
+		for (size_t j = 0; j < c; j++){
+			int alive = nbrCount(i, j, W);
+			if ((W[i][j] == 1) && (alive < 2 || alive > 3)){
+				newGen[i][j] = 0;
+			}
+			else if ((W[i][j] == 0) && (alive == 3)){
+				newGen[i][j] = 1;
+			}
+		}
+	}
+	W = newGen;
+}
+
+void initFromFile(const string fname) {
+    //Initializes board from a file
+    FILE* f = fopen(fname.c_str(),"rb");
+    if (!f) {
+        exit(1);
+    }
+    //Reinitialiaze world
+    W.clear();
+    W.push_back(vector<bool>());
+    size_t rows = 0;
+    //Read character by character and input into world
+    char c;
+    while (fread(&c,1,1,f)) {
+    if (c == '\n') {
+				//New line, increment row
+        rows++;
+        W.push_back(vector<bool>());
+    } else if (c == '.') {
+        W[rows].push_back(false); /* dead x_x */
+    } else {
+        W[rows].push_back(true); /* alive 8D */
+    }
+    }
+    fclose(f);
+    //Remove last row (if empty?)
+    W.pop_back();
+}
+
 void mainLoop() {
-	/* TODO: write this */
-	/* update, write, sleep */
+	FILE* f;
+	if (wfilename == "-"){
+		f = stdout;
+	}
+	else {
+		f = fopen(wfilename.c_str(), "wb");
+	}
+
 	if (max_gen == 0) {
-		/* make one generation update per second */
-	} else {
-		/* go through generations as fast as you can until
-		 * max_gen is reached... */
+		while(true){
+			sleep(1);
+			update();
+			dumpState(f);
+		}
+	}
+	else {
+		for (size_t x = 0; x < max_gen; x++)
+		{
+			update();
+		}
+		dumpState(f);
+	}
+	fclose(f);
+}
+
+void dumpState(FILE* f)
+{
+	rewind(f);
+	size_t r = W.size();
+	size_t c = W[0].size();
+	char ch;
+	for (size_t x = 0; x < r; x++){
+		for (size_t y = 0; y < c; y++){
+			if (W[x][y] == 0){
+				ch = '.';
+				fwrite(&ch,1,1,f);
+			}
+			else if (W[x][y] == 1){
+				ch = 'O';
+				fwrite(&ch,1,1,f);
+			}
+		}
+		ch = '\n';
+		fwrite(&ch,1,1,f);
 	}
 }
+
+
+
+
+
